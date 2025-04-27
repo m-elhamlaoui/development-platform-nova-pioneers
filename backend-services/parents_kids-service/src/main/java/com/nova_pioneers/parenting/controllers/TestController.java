9c760e4b2c1b49aeeeae496ca9d62d3823674d65
Abderrahmane <essahihabderrahman2020@gmail.com>
2025-04-27 17:38:00 +0100

    Refactor: Converted microservices to individual Spring Boot apps, added Dockerfiles and modified Docker Compose.

---
+++ backend-services/parents_kids-service/src/main/java/com/nova_pioneers/parenting/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.nova_pioneers.admin.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/parenting/test")
+    public String testEndpoint() {
+        return "Parenting service working";
+    }
+}
