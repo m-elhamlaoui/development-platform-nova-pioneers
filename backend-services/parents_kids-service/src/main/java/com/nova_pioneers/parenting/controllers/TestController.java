b38fe6c6f8ddb9b5e1a3b919dd46b1d1cd2a065b
Abderrahmane <essahihabderrahman2020@gmail.com>
2025-04-30 12:42:34 +0100

    added the api gateway

--- backend-services/parents_kids-service/src/main/java/com/nova_pioneers/parenting/controllers/TestController.java
+++ backend-services/parents_kids-service/src/main/java/com/nova_pioneers/parenting/controllers/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/api/parenting/test")
+    @GetMapping("/api/test")
     public String testEndpoint() {
         return "Parenting service working";
     }
--- backend-services/teachers_courses-service/src/main/java/com/nova_pioneers/teaching/controllers/TestController.java
+++ backend-services/teachers_courses-service/src/main/java/com/nova_pioneers/teaching/controllers/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/api/Teaching/test")
+    @GetMapping("/api/test")
     public String testEndpoint() {
         return "Teaching Service Working";
     }
